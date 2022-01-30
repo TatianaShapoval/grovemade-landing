a084f65c8a2bfda5581f020d663bdbaac8255742
TatianaShapoval <questopunto@gmail.com>
2022-01-30 03:05:53 +0200

    added adaptive part 1

--- js/main.js
+++ js/main.js
@@ -5,5 +5,31 @@
         slidesToShow: 4,
         slidesToScroll: 3,
         dots: true,
+        responsive: [
+            {
+              breakpoint: 676,
+              settings: {
+                slidesToScroll: 3,
+                slidesToShow: 3,
+              }
+              
+            },         
+            {
+              breakpoint: 475,
+              settings: {
+                slidesToScroll: 4,
+                slidesToShow: 2,
+              }
+              
+            },
+            {
+                breakpoint: 414,
+                settings: {
+                  slidesToScroll: 6,
+                  slidesToShow: 1,
+                }
+                
+              }
+          ]
     });
   
